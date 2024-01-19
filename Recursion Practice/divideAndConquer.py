c2796232e6f10068757f5082267af02e729592e6
Smurvy <riley@motylinski.com>
2024-01-19 11:17:51 -0500

    Update divideAndConquer.py

--- Recursion Practice/divideAndConquer.py
+++ Recursion Practice/divideAndConquer.py
@@ -1,14 +1,13 @@
-# how to find index??
 
 def divideAndConquer(myList, targetNum):
     middlePos = int(len(myList)/2)
     if(myList[len(myList) - 1] == targetNum):
-        return len(myList)
+        return 0
 
     if(myList[middlePos] > targetNum):
         return divideAndConquer(myList[:middlePos],targetNum)
     else:
-        return divideAndConquer(myList[middlePos:],targetNum)
+        return middlePos + divideAndConquer(myList[middlePos:],targetNum)
 
-print(divideAndConquer([1,2,3,4,5],5))
+print(divideAndConquer([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],2))
 
