1689b07be504f5c106d5490b835d0d8138673fba
Smurvy <75571383+Smurvy@users.noreply.github.com>
2024-02-28 21:45:16 -0500

    added pop function to doubly llinked list

--- dsImplementations/doublyLinkedList/doublyLinkedList.c
+++ dsImplementations/doublyLinkedList/doublyLinkedList.c
@@ -23,6 +23,22 @@ struct Node addNode(struct Node* oldNode, int val){
 
 }
 
+struct Node pop(struct Node node1){
+    if(node1.forwardPointer == NULL){
+        if(node1.backwardPointer != NULL){
+       
+            // need to "unlink" the node we are returnign from the previous node
+            node1.backwardPointer->forwardPointer = NULL;
+            node1.backwardPointer = NULL;
+        }
+        
+        return node1;
+    }  else {
+        pop(*node1.forwardPointer);
+    }
+
+}
+
 
 
 void printLinkedList(struct Node node){
@@ -39,12 +55,15 @@ void printLinkedList(struct Node node){
 int main() {
     // if no value is set, the pointers automatically point to itself
     struct Node node1;
+    node1.backwardPointer = NULL;
+    node1.forwardPointer = NULL;
     node1.number = 7;
-
+    
 
     struct Node node2 = addNode(&node1,8);
     struct Node node3 = addNode(&node2,9);
 
+    pop(node1);
 
     printLinkedList(node1);
     return 0;
